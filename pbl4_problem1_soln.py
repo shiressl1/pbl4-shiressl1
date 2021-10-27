a7ae199cb5bad7cc8ab4d552749771e06a887b22
Sarah Sanders <72199180+shiressl1@users.noreply.github.com>
2021-10-27 17:34:13 +0000

    added replit badge

--- pbl4_problem1_soln.py
+++ pbl4_problem1_soln.py
@@ -1,9 +1,7 @@
 # Problem 1
 import pandas as pd
 
-df = pd.read_csv(
-    'https://raw.githubusercontent.com/nicholsons/comp0035_pbl4/master/data/paralympics_raw.csv?token'
-    '=AHBUVRXMMNPI7VSWY4P34IDBO2TES')
+df = pd.read_csv('data/paralympics_raw.csv')
 
 pd.set_option('display.max_rows', df.shape[0] + 1)
 pd.set_option('display.max_columns', df.shape[1] + 1)
